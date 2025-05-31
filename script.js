a5afb6d21c3600802efad1d01f933ed8c70523b4
HimashaDewmini <himashadewmini12345@gmail.com>
2025-05-31 14:00:41 +0530

    update css code

--- script.js
+++ script.js
@@ -40,9 +40,11 @@
     }
     const hamburger = document.getElementById('hamburger');
     const navLinks = document.getElementById('navLinks');
+    
 
     hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('show');
+   
   });
 
     displayPets(pets);
